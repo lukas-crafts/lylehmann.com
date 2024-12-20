d5ca6f4159f83ac36545c192f90362e52700e0d0
Lukas Yair Lehmann <llehmann84@outlook.com>
2024-12-20 16:03:57 +0100

    updated landing page

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,5 +1,7 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
+const currentYear = new Date().getFullYear();
+
 export const headerData = {
   links: [
     {
@@ -120,64 +122,57 @@ export const headerData = {
 
 export const footerData = {
   links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
+    { text: 'Home', href: '/' },
+    { text: 'About', href: '/about' },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: 'Impressum', href: getPermalink('/impressum') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    {
+      ariaLabel: 'Linkedin',
+      icon: 'tabler:brand-linkedin',
+      href: 'https://linkedin.com/in/lylehmann',
+      target: '_blank',
+    },
+    { ariaLabel: 'GitHub', icon: 'tabler:brand-github', href: 'https://github.com/lylehmann' },
+  ],
+  footNote: `&copy; ${currentYear} Lukas Yair Lehmann. All rights reserved.`,
+};
+
+export const navigation = {
+  secondaryLinks: [
+    {
+      text: 'Terms',
+      href: getPermalink('/terms'),
+    },
+    {
+      text: 'Privacy policy',
+      href: getPermalink('/privacy'),
+    },
+  ],
+  socialLinks: [
+    {
+      ariaLabel: 'Linkedin',
+      icon: 'tabler:brand-linkedin',
+      href: 'https://linkedin.com/in/lylehmann',
+      target: '_blank',
+    },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/lylehmann' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    &copy; ${currentYear} Lukas Yair Lehmann. Template made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
   `,
 };
+
+export interface Props {
+  links: { text: string; href: string }[];
+  secondaryLinks: { text: string; href: string }[];
+  socialLinks: (
+    | { ariaLabel: string; icon: string; href: string; target: string }
+    | { ariaLabel: string; icon: string; href: string; target?: undefined }
+  )[];
+  footNote: string;
+}
