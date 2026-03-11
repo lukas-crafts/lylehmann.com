288465fcc59681c421e93f7c0c8d9c91cef50550
Lukas Yair Lehmann <llehmann84@outlook.com>
2026-03-11 15:13:32 +0100

    chore: final polish and lint fixes

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -102,7 +102,7 @@ export const adaptOpenGraphImages = async (
   const defaultHeight = 626;
 
   const adaptedImages = await Promise.all(
-    images.map(async (image) => {
+    images.map(async (image: any) => {
       if (image?.url) {
         const resolvedImage = (await findImage(image.url)) as
           | ImageMetadata
