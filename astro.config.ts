0a0e5aa81998f1b23a17f00bbc95994dbfd37719
Lukas Yair Lehmann <llehmann84@outlook.com>
2026-03-13 22:00:19 +0100

    Refactor code structure for improved readability and maintainability

--- astro.config.ts
+++ astro.config.ts
@@ -79,7 +79,11 @@ export default defineConfig({
   ],
 
   image: {
-    remotePatterns: [{ protocol: "https", hostname: "cdn.pixabay.com" }],
+    remotePatterns: [
+      { protocol: "https", hostname: "cdn.pixabay.com" },
+      { protocol: "https", hostname: "images.pexels.com" },
+      { protocol: "https", hostname: "plus.unsplash.com" },
+    ],
   },
 
   markdown: {
